8e95ed7cf6687a3000c24ce28073e092af8ea5c6
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-09-25 10:54:11 -0700

    update: simple string

--- server/src/main/java/com/example/server/controllers/HelloController.java
+++ server/src/main/java/com/example/server/controllers/HelloController.java
@@ -3,13 +3,11 @@ package com.example.server.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public Map<String, String> hello() {
-        return Map.of("message", "Hello, World!");
+    public String hello() {
+        return "Hello, world!";
     }
 }
